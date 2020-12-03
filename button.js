0439c20941f818fe905d62c3cf6fff8bd3ee4340
Jason Panzer <jason.drew.panzer@gmail.com>
2020-12-02 22:10:23 -0800

    make a network call to update an element

--- button.js
+++ button.js
@@ -1,6 +1,23 @@
 function changeColor() {
   var blueDiv = document.getElementById("changeable");
-  blueDiv.onclick = function () {
-    blueDiv.style.backgroundColor = "lightgreen";
-  };
+  blueDiv.style.backgroundColor = "lightgreen";
+}
+
+function replaceWord() {
+  var wordable = document.getElementById('wordable');
+
+  fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "your-api-key-here",
+      "x-rapidapi-host": "api-host-here",
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      response.json().then(json => wordable.innerText = json.word);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
