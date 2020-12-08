4bd4ae32ef715acd550f831f54d1961462645bfe
Jason Panzer <jason.drew.panzer@gmail.com>
2020-12-08 04:25:58 -0800

    Create examples

--- languageFeatures.js
+++ languageFeatures.js
@@ -1,9 +1,141 @@
-/** console.log */
-/** var, let, const, scope implications */
-/** string concat and template string */
-/** types and objects, arrays */
-/** function declarations ES5 to ES6 */
-/** closures and attaching member to functions */
+/**
+ * Code Formatting
+ * style guide?
+ *
+ */
+
+/**
+ * `console.log`
+ * When you're trying to debug some code you've written, the very most basic
+ * way to get feedback is to print information out to the console. If you're
+ * running Node in some server or from the command line, that output might
+ * display in the terminal. In a browser, you'll view it through developer
+ * tools.
+ */
+
+function stdOut(someValue) {
+  console.log(someValue);
+}
+
+/**
+ * var, let, const
+ * In ES5 (an earlier language specification), you could declare a variable with
+ * `var`.  In JavaScript, variables declared with `var` are scoped to the
+ * nearest function.
+ */
+
+function scopeItOut() {
+  if (true) {
+    var x = 42;
+  }
+  console.log(x); // prints 42
+}
+
+/**
+ * ES6 introduces two new options: `let` and `const`. They both are scoped to
+ * the nearest block. Note that `const` creates a constant which cannot be later
+ * overwritten.
+ */
+function scopeItIn() {
+  const y = 32;
+  if (!false) {
+    let z = "stringy";
+  }
+  console.log(z); // error
+  y = "another string";
+}
+
+/**
+ * Function Declarations
+ * In ES5, we declare functions much like those above, using the keyword
+ * `function`, some identifier and may have parameters.
+ */
+
+function theBest(first, second, last) {
+  return first + second + last;
+}
+
+/**
+ * ES6 introduces other options: arrow funtions and default parameters.
+ */
+
+const arrowFunc = (aThing = "default", anotherThing, aThirdThing) => {
+  return aThing + anotherThing + aThirdThing;
+};
+
+/**
+ * Types, Objects, Collections
+ * In Java and other strongly-typed languages, a variable must have a specific
+ * type. This is not true in Javascript.
+ */
+
+const changingTypes = () => {
+  let foo = 20; // It's a number
+  foo = "Whan that Aprill with his shoures soote"; // It's a string
+  foo = true; // And now it's a boolean
+  console.log(typeof foo);
+};
+
+/**
+ * A JavaScript object is very similar to a dictionary in other languages. In
+ * point of fact, most of what we'd consider collections (maps, arrays, sets) in
+ * other languages are objects in JavaScript.
+ */
+
+const object = {
+  key: "a string value",
+  validKey: 42,
+  truth: false,
+  aFunc: changingTypes,
+};
+
+// Note that we've just assigned a variable to a key in this object.
+
+object.aFunc();
+
+const anArray = [82, "heterogenous", object];
+
+/**
+ * Closures
+ * JavaScript closures allow functions to access variables in scope when the
+ * function was defined.
+ */
+
+const myCounter = () => {
+  let counter = 0;
+  return {
+    increment: (inc) => {
+      counter += inc;
+    },
+    getValue: () => {
+      return counter;
+    },
+  };
+};
+
+/** Strings */
+// String concatenation
+// Template string
+
+/**
+ * Control Statements, Truthiness/Falsiness
+ * JavaScript provides the kind of control structures we'd expect: if/else
+ * statements, while loops, try/catch/finally. In addition, JavaScript has a
+ * novel way of evaluating true and false for values.
+ */
+
+//Some Values are Falsey and Truthy
+// Falsey - evaluates to false
+var isNull = null;
+var isUndef = undefined;
+var isFalse = false;
+var isZero = 0;
+
+// Truthy - evaluates to true
+var isDef = "Hello, world!";
+var isTrue = true;
+var isNotZero = 1;
+
 /** synchronous/asynchronous */
 /** promises and async/await */
 /** modules (with this as an example) */
--- simpleServer.js
+++ simpleServer.js
@@ -1,4 +1,59 @@
-/** One of the important benefits of Node is that it provides a host of tools
+/** 
+ * One of the important benefits of Node is that it provides a host of tools
  * for rapidly developing backend services. Taking advantage of these libraries
  * is relatively painless.
+ * 
+ * First, we need to import the tools we need from Node's http library.
  */
+
+const http = require('http');
+
+/** 
+ * Now we set up the function that will handle incoming requests. We save this
+ * function as a variable so that we can pass it as a parameter later.
+ * 
+ * This function expects two arguments, representing the request and response
+ * objects. 
+ */
+const requestListener = function (req, res) {
+  /** 
+   * These objects are how we interact with the request-response cycle. For 
+   * instance, we may want to read data from the request body. In this case
+   * we listen for the `data` event. When it occurs, we print out a chunk of
+   * data to the console.
+   */
+  req.on('data', chunk => {
+    console.log(`Data chunk available: ${chunk}`)
+  })
+
+  /** 
+   * When the `end` event fires, we know we've reached the end of the data and
+   * can take whatever action we think is appropriate.
+   */
+  req.on('end', () => {
+    //do something
+  })
+
+  /** 
+   * Of course, it wouldn't be a request-response cycle without a response. We 
+   * can write the status code and message payload for the response here.
+   */
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
+
+/**
+ * We pass this listener function to the http server, which will invoke it any
+ * time a request is received. Node keeps track of callbacks like this and 
+ * activates them at the appropriate time. This is a trivial example of the 
+ * event loop, at the core of Node.
+ */
+
+const server = http.createServer(requestListener);
+
+/** 
+ * Last, but not least, we have to tell the server which port to listen on. Much
+ * like our examples of method chanining, this is invoking the `listen` function
+ * on the server object we've created.
+ */
+server.listen(8080);
