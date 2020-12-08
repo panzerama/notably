b697c51e1cedf15ad355ac2ea08d5611d1391aca
Jason Panzer <jason.drew.panzer@gmail.com>
2020-12-07 22:19:22 -0800

    start simple server example

---
+++ simpleServer.js
@@ -0,0 +1,4 @@
+/** One of the important benefits of Node is that it provides a host of tools
+ * for rapidly developing backend services. Taking advantage of these libraries
+ * is relatively painless.
+ */
